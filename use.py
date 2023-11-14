80897c89a3ce6c646b2e7b8a6b1ffee48d8ea530
Suyash Bagwe <suyashbagwe717@gmail.com>
2023-11-14 17:04:43 +0530

    ignore

--- use.py
+++ use.py
@@ -1,23 +1,19 @@
 import tensorflow as tf
-import os
 
-# Specify the directory path where the USE model is saved
+
 saved_dir = 'USE'
 
-# Load the USE model
 use_model = tf.saved_model.load(saved_dir)
 
-# Define sentences to be evaluated
 sentences = [
     "My name is suyash.",
     "That boy's name is suyash.",
     "He studies in APSIT."
 ]
 
-# Encode the sentences
 embeddings = use_model(sentences)
 
-# Calculate the cosine similarity between sentences
+
 
 for i in range(len(sentences)):
     for j in range(i + 1, len(sentences)):
